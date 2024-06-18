78f4b0711c896e5a0252c7cfc99a2b1abaf30b83
oluwatobi <oluwatobiajayi001@gmail.com>
2024-06-18 23:04:48 +0100

    new file:   .gitignore 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   tailwind.config.js 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl text-red-500 bg-gradient-to-t from-red-300 to-red-600">
+        HELLO!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
